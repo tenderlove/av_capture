45499a3017bae99cb60e50f49f8c18b7dd4f4fce
Aaron Patterson <aaron.patterson@gmail.com>
2013-11-04 10:28:15 -0800

    getting a connection works

--- test/test_maccam.rb
+++ test/test_maccam.rb
@@ -7,18 +7,28 @@ class TestMaccam < MiniTest::Unit::TestCase
     assert_equal 0, sio.connections.length
   end
 
-  def test_connect
-    skip "not working yet"
-    sio = AVCapture::StillImageOutput.new
-    assert_equal 0, sio.connections.length
-    assert sio.connect AVCapture::AVMediaTypeVideo
-    assert_equal 1, sio.connections.length
-  end
-
   def test_session
     session = AVCapture::Session.new
     sio = AVCapture::StillImageOutput.new
     assert session.can_add_output?(sio)
     session.add_output sio
+  end
+
+  def test_add_input
+    session = AVCapture::Session.new
+    dev     = AVCapture.devices[1]
+    input   = AVCapture::DeviceInput.new dev
+    assert session.can_add_input?(input)
+    session.add_input input
+  end
+
+  def test_add_io
+    session = AVCapture::Session.new
+    dev     = AVCapture.devices[1]
+    input   = AVCapture::DeviceInput.new dev
+    output  = AVCapture::StillImageOutput.new
+    session.add_input input
+    session.add_output output
+    assert_equal 1, output.connections.length
   end
 end
